c8ae6e21a3f1d63080641d70bdbad2a0a713313d
peter kuria <you@example.com>
2019-12-13 04:23:02 -0500

    defined a new function password_generation to automatically generate password

--- credentials.py
+++ credentials.py
@@ -9,6 +9,13 @@ def create_new_user(first_name,last_name,password):
      '''a function to save a newly created user'''
      User.user_save(user)
   def authenticate_user(f_name,password):
-      '''function to authenticate user before accontion creation'''
+      '''function to authenticate user before account login'''
       qualify_user=Credentials.user_auth(f_name,password)
-    return qualify_user       
+    return qualify_user
+def password_generation():
+    '''a function to generate new password automatically"
+
+    new_pass=Credentials.pass_gen()
+    return new_pass
+
+    
