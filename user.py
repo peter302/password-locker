2f638ef5c6f69b094ccb2f1980fec65489317083
peter kuria <you@example.com>
2019-12-13 02:45:09 -0500

    defined a blueprint user class and initialized it with usernames and password

---
+++ user.py
@@ -0,0 +1,8 @@
+import pyperclip,string,random
+ class User:
+     user_list=[] #a list to hold user details
+     def __init__(self,f_name,l_name,password):
+         "a method acting as constructor to initalize instances of a class"
+         self.f_name=f_name
+         self.l_name=l_name
+         self.password=password
