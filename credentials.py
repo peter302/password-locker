a93c9251cdd8f4d452ae69688af047d9bd267c14
peter kuria <you@example.com>
2019-12-13 04:07:36 -0500

    added a create_new_user function to create a new user object

--- credentials.py
+++ credentials.py
@@ -1,2 +1,7 @@
 import pyperclip
 from user import User, Credentials
+
+def create_new_user(first_name,last_name,password):
+   '''function to create a new user'''
+   new_user=user(first_name,last_name,password)
+   return new_user
--- user.py
+++ user.py
@@ -1,15 +1,14 @@
 import pyperclip,string,random
- class User:
+class User:
      user_list=[] #a list to hold user details
      def __init__(self,f_name,l_name,password):
          "a method acting as constructor to initalize instances of a class"
          self.f_name=f_name
          self.l_name=l_name
          self.password=password
-    def user_save(self):
+     def user_save(self):
         "a method to save a new user object when ceated"
         User.user_list.append(self)
-
 class Credentials:
         "this class will hold users details including names and passwords for user's site plus funtions and method to alter those dtails"
 
