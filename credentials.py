7ee7eadadc67ab332eda95760e9de32c5b162b9c
peter kuria <you@example.com>
2019-12-13 04:11:05 -0500

    added a new function save_user to save a new function

--- credentials.py
+++ credentials.py
@@ -5,3 +5,7 @@ def create_new_user(first_name,last_name,password):
    '''function to create a new user'''
    new_user=user(first_name,last_name,password)
    return new_user
+ def save_user(user):
+     '''a function to save a newly created user'''
+     User.user_save(user)
+     
