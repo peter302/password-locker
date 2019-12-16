b4a3c6bd20c8f1b9d65266a9a5c30fd671a69641
peter kuria <you@example.com>
2019-12-15 23:19:19 -0500

    defined a new method to  check if the new user has been succesfuly saved in to the users list

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.f_name,'peter')
         self.assertEqual(self.new_user.l_name,'kuria')
         self.assertEqual(self.new_user.password,'0000')
+    def test_user_save(self):
+        '''this methods checks if the new user has been succesfuly saved in to the users list'''
+        self.new_user.user_save()
+        self.assertEqual(len(user.user_list),1)
+class TestCredential(unittest.TestCase):
