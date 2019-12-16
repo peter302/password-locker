1fa10dafb6aa95511044543c5673a6212fe13fbc
peter302 <38464892+peter302@users.noreply.github.com>
2019-12-16 09:16:33 -0500

    modified user object

--- credentials.py
+++ credentials.py
@@ -3,7 +3,7 @@ from user import User, Credentials
 
 def create_new_user(first_name,last_name,password):
    '''function to create a new user'''
-   new_user=user(first_name,last_name,password)
+   new_user=User(first_name,last_name,password)
    return new_user
 def save_user(user):
      '''a function to save a newly created user'''
