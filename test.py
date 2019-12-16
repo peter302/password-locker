ce70b476c5fe9cf9b3b73a6ec8a8ba4140cab58f
peter kuria <you@example.com>
2019-12-15 23:07:22 -0500

    added a new class to test behaviours of user class

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import unittest
 import pyperclip
 from user import User,Credentials
+
+class TestUser(unittest.TestCase):
+    '''this class tests methods for user'''
+
+    def setUp(self):
+        '''this method runs before each test case'''
